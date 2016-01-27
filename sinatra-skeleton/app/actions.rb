34060590dddf0259907646e2b851d51a5836b97e
plaeski <lisa.plaseski@gmail.com>
2016-01-27 14:30:26 -0500

    Set up basic paths

--- sinatra-skeleton/app/actions.rb
+++ sinatra-skeleton/app/actions.rb
@@ -10,3 +10,16 @@ end
 get '/user/sign_up' do
 	erb :'/user/sign_up/index'
 end
+
+get '/groups' do
+ erb :'/groups/index'
+end
+
+get '/events' do
+ erb :'/events/index'
+end
+
+get '/services' do
+ erb :'/services/index'
+end
+
