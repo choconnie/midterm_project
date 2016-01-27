4f3607ac0f33718f0317019b5ef304f2c120b473
choconnie <hj.c.cho@gmail.com>
2016-01-27 19:54:56 +0000

    db

--- sinatra-skeleton/db/schema.rb
+++ sinatra-skeleton/db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,11 +11,67 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140321144528) do
+ActiveRecord::Schema.define(version: 20160127191448) do
 
-  create_table "users", force: true do |t|
-    t.string "name"
-    t.string "email"
+  create_table "comments", force: :cascade do |t|
+    t.integer  "post_id"
+    t.string   "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
+
+  create_table "events", force: :cascade do |t|
+    t.integer  "post_id"
+    t.string   "title"
+    t.string   "event_date"
+    t.string   "location"
+    t.string   "url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "events", ["post_id"], name: "index_events_on_post_id"
+
+  create_table "groups", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "group_name"
+    t.string   "city"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "groups", ["user_id"], name: "index_groups_on_user_id"
+
+  create_table "posts", force: :cascade do |t|
+    t.integer  "group_id"
+    t.string   "title"
+    t.string   "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "posts", ["group_id"], name: "index_posts_on_group_id"
+
+  create_table "servicies", force: :cascade do |t|
+    t.string   "title"
+    t.string   "content"
+    t.string   "email"
+    t.string   "phone"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.integer  "group_id"
+    t.string   "username"
+    t.string   "email"
+    t.boolean  "status",     default: true
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["group_id"], name: "index_users_on_group_id"
 
 end
