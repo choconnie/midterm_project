631193ab881b6e7cef3b3c046165913e76e80e7e
Charlotte <char.lopez11@gmail.com>
2016-02-01 14:13:52 +0000

    added contacts view

---
+++ app/models/contacts.rb
@@ -0,0 +1,7 @@
+class Contact < ActiveRecord::Base
+
+  validates :name, presence: true
+  validates :email, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
+  validates :message, presence: true
+
+end
