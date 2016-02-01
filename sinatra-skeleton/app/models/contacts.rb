8e3419ba351daa5cdcc0d49965f6ca8aac6223ae
plaeski <lisa.plaseski@gmail.com>
2016-02-01 08:55:52 -0500

    Removed duplicate files

--- sinatra-skeleton/app/models/contacts.rb
+++
@@ -1,7 +0,0 @@
-class Contact < ActiveRecord::Base
-
-  validates :name, presence: true
-  validates :email, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
-  validates :message, presence: true
-
-end
