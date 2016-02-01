a1e8bff58b4ddf721f0a81dee73f100a428222a9
Charlotte <char.lopez11@gmail.com>
2016-02-01 00:19:42 +0000

    add migrate files

---
+++ sinatra-skeleton/db/migrate/20160131210759_add_contacts.rb
@@ -0,0 +1,10 @@
+class AddContacts < ActiveRecord::Migration
+  def change
+    create_table :contacts do |t|
+      t.string :name
+      t.string :email
+      t.string :message
+      t.timestamps
+    end
+  end
+end
