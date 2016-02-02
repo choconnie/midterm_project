fbe0c68c525326ab1c42bf018dd2dee22cb60f84
Charlotte <char.lopez11@gmail.com>
2016-02-02 19:55:00 +0000

    add db url

--- config/database.rb
+++ config/database.rb
@@ -2,7 +2,6 @@ configure do
   # Log queries to STDOUT in development
   if Sinatra::Application.development?
     ActiveRecord::Base.logger = Logger.new(STDOUT)
-  end
 
   set :database, {
     adapter: "sqlite3",
