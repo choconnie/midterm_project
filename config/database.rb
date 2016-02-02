9c716942579976a8198f72be972717f5a42e0b8d
Charlotte <char.lopez11@gmail.com>
2016-02-02 19:41:51 +0000

    changed to timestampz

--- config/database.rb
+++ config/database.rb
@@ -1,12 +1,19 @@
 configure do
   # Log queries to STDOUT in development
-  if development?
-    set :database, {
-      adapter: "sqlite3",
-      database: "db/db.sqlite3"
-  }
-  else
-    set :database, ENV['postgres://sbjlqagqrcjfpm:ckAnxPSIiWnwtT71nloEkBk3bV@ec2-54-235-152-114.compute-1.amazonaws.com:5432/dfnk5l9ejko6jh']
+  if Sinatra::Application.development?
+    ActiveRecord::Base.logger = Logger.new(STDOUT)
   end
-  
+
+  set :database, {
+    adapter: "sqlite3",
+    database: "db/db.sqlite3"
+  }
+
+  # Load all models from app/models, using autoload instead of require
+  # See http://www.rubyinside.com/ruby-techniques-revealed-autoload-1652.html
+  Dir[APP_ROOT.join('app', 'models', '*.rb')].each do |model_file|
+    filename = File.basename(model_file).gsub('.rb', '')
+    autoload ActiveSupport::Inflector.camelize(filename), model_file
+  end
+
 end
--- db/migrate/20160129022015_change_event_date_column_to_date.rb
+++ db/migrate/20160129022015_change_event_date_column_to_date.rb
@@ -1,5 +1,5 @@
 class ChangeEventDateColumnToDate < ActiveRecord::Migration
   def change
-  	change_column :events, :event_date, :datetime
+  	change_column :events, :event_date, :datetime, :timestampz
   end
 end
