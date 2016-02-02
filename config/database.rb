4b3eef9bb0f07404c9b1d4e2eb4f723430714c60
Charlotte <char.lopez11@gmail.com>
2016-02-02 19:01:29 +0000

    transfer to heroku

--- config/database.rb
+++ config/database.rb
@@ -6,7 +6,7 @@ configure do
       database: "db/db.sqlite3"
   }
   else
-    set :database, ENV['DATABASE_URL']
+    set :database, ENV['postgres://sbjlqagqrcjfpm:ckAnxPSIiWnwtT71nloEkBk3bV@ec2-54-235-152-114.compute-1.amazonaws.com:5432/dfnk5l9ejko6jh']
   end
   
 end
